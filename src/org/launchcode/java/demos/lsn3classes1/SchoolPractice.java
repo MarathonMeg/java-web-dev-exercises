92688a2b82722d07eac51454b778b9c5cb099473
Megan Albin <albin.megan@gmail.com>
2022-03-27 11:33:23 -0500

    "Chapter 4 Exercises"

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,74 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import org.launchcode.java.demos.lsn4classes2.Student;
+
+import java.util.ArrayList;
+
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+
+    }
+
+    private class Course {
+        private String courseName;
+        private String teacherName;
+        private ArrayList<Student> enrolledStudents;
+
+        //must make a course Object out of the Class fields
+        public Course(String courseName, String teacherName, ArrayList<Student> enrolledStudents) {
+            this.courseName = courseName;
+            //I want to retrieve the teacher name from the Teacher Class
+            this.teacherName = teacherName;
+            //not sure how to fetch the student list from the Student file
+
+        }
+
+        private class Teacher {
+            private String firstName;
+            private String lastName;
+            private String subject;
+            private int yearsTeaching;
+
+            //must make a teacher Object out of the Class fields
+            public Teacher(String firstName, String lastName, String subject, int yearsTeaching) {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.subject = subject;
+                this.yearsTeaching = yearsTeaching;
+            }
+
+            public String getFirstName() {
+                return firstName;
+            }
+
+            public void setFirstName(String firstName) {
+                this.firstName = firstName;
+            }
+
+            public String getLastName() {
+                return lastName;
+            }
+
+            public void setLastName(String lastName) {
+                this.lastName = lastName;
+            }
+
+            public String getSubject() {
+                return subject;
+            }
+
+            public void setSubject(String subject) {
+                this.subject = subject;
+            }
+
+            public int getYearsTeaching() {
+                return yearsTeaching;
+            }
+
+            public void setYearsTeaching(int yearsTeaching) {
+                this.yearsTeaching = yearsTeaching;
+            }
+        }
     }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,59 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0.0);
+    }
+    //Student megan = new Student("Megan", 123, 1, 4.0);
+
+    //getter/setter Name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //getter/setter studentId
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    //getter/setter number of credits
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    //getter/setter gpa
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+
+    public static void main(String[] args) {
+        Student megan = new Student("Megan", 123, 1, 4.0);
+
+
+    }
 }
+
